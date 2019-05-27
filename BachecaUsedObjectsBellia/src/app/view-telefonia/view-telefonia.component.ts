9b8f22d79fa87ac08f24ed6b513eee6a1d958d52
EC2 Default User <ec2-user@ip-172-31-81-120.ec2.internal>
2019-05-27 07:00:26 +0000

    4

--- BachecaUsedObjectsBellia/src/app/view-telefonia/view-telefonia.component.ts
+++ BachecaUsedObjectsBellia/src/app/view-telefonia/view-telefonia.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Telefoni } from '../telefoni.model';
 
 @Component({
   selector: 'app-view-telefonia',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-telefonia.component.css']
 })
 export class ViewTelefoniaComponent implements OnInit {
+  
+  @Input() telefono: Telefoni;
 
   constructor() { }
 
