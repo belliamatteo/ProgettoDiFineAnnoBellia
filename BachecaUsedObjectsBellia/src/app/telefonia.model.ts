87c9f91920e12207538dd9ed0df24b111b07814b
belliamatteo <bellia138@gmail.com>
2019-09-10 13:44:51 +0000

    finito

--- BachecaUsedObjectsBellia/src/app/telefonia.model.ts
+++
@@ -1,6 +0,0 @@
-export class Telefoni
-{
-    public nome:string;
-    public cognome : string;
-    public email : string;
-}
--- BachecaUsedObjectsBellia/src/app/telefoniaList.model.ts
+++
@@ -1,8 +0,0 @@
-import { Telefoni } from "./telefonia.model";
-
-export const TelefoniaList : Telefoni[] =
-[
-    {nome:"Marco", cognome: "Rossi", email: "m.rossi44@gmail.com"},
-    {nome:"Luca", cognome: "Sarri", email: "l.sarri34@gmail.com"},
-    {nome:"Gaia", cognome: "Ombra", email: "g.ombra24@gmail.com"},
-]
